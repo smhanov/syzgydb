6c84b4db1eedda1ecbf66ce6e1e69253da4c1245
Steve Hanov <steve.hanov@gmail.com>
2024-10-01 16:59:24 -0400

    feat: add collection options, document management, and distance calculation methods

--- collection.go
+++ collection.go
@@ -10,23 +10,68 @@ import (
 	"sync"
 )
 
-type DistanceIndex struct {
+type CollectionOptions struct {
+	Name           string
+	DistanceMethod int
+	DimensionCount int
+	Quantization   int
+}
+
+type Document struct {
+	ID       uint64
+	Vector   []float64
+	Metadata []byte
+}
+
+type SearchResult struct {
+	ID       uint64
+	Metadata []byte
+	Distance float64
+}
+
+type SearchResults struct {
+	Results []SearchResult
+
+	// percentage of database searched
+	PercentSearched float64
+}
+
+type SearchArgs struct {
+	Vector []float64
+	Filter FilterFn
+
+	// for nearest neighbour search
+	MaxCount int
+
+	// for radius search
+	Radius float64
+}
+
+type FilterFn func(id uint64, metadata []byte) bool
+
+// 4 bytes: version
+// 4 bytes: length of the header
+// 1 byte: distance method
+// 4 bytes: number of dimensions
+const headerSize = 14 // Update the header size to 14
+
+type distanceIndex struct {
 	distance     float64
 	index        uint64
 	Quantization int // Add this line
 }
 
-type ApproxHeap []DistanceIndex
+type approxHeap []distanceIndex
 
-func (h ApproxHeap) Len() int           { return len(h) }
-func (h ApproxHeap) Less(i, j int) bool { return h[i].distance < h[j].distance }
-func (h ApproxHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h approxHeap) Len() int           { return len(h) }
+func (h approxHeap) Less(i, j int) bool { return h[i].distance < h[j].distance }
+func (h approxHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
 
-func (h *ApproxHeap) Push(x interface{}) {
-	*h = append(*h, x.(DistanceIndex))
+func (h *approxHeap) Push(x interface{}) {
+	*h = append(*h, x.(distanceIndex))
 }
 
-func (h *ApproxHeap) Pop() interface{} {
+func (h *approxHeap) Pop() interface{} {
 	old := *h
 	n := len(old)
 	x := old[n-1]
@@ -34,17 +79,17 @@ func (h *ApproxHeap) Pop() interface{} {
 	return x
 }
 
-type ResultHeap []SearchResult
+type resultHeap []SearchResult
 
-func (h ResultHeap) Len() int           { return len(h) }
-func (h ResultHeap) Less(i, j int) bool { return h[i].Distance > h[j].Distance }
-func (h ResultHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h resultHeap) Len() int           { return len(h) }
+func (h resultHeap) Less(i, j int) bool { return h[i].Distance > h[j].Distance }
+func (h resultHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
 
-func (h *ResultHeap) Push(x interface{}) {
+func (h *resultHeap) Push(x interface{}) {
 	*h = append(*h, x.(SearchResult))
 }
 
-func (h *ResultHeap) Pop() interface{} {
+func (h *resultHeap) Pop() interface{} {
 	old := *h
 	n := len(old)
 	x := old[n-1]
@@ -62,6 +107,69 @@ type Collection struct {
 	memfile       *memfile
 	pivotsManager PivotsManager
 	mutex         sync.Mutex
+}
+
+func NewCollection(options CollectionOptions) *Collection {
+	distanceFn := euclideanDistance
+	if options.DistanceMethod == Cosine {
+		distanceFn = cosineDistance
+	}
+
+	// Validate and set Quantization
+	if options.Quantization == 0 {
+		options.Quantization = 64
+	} else if options.Quantization != 4 && options.Quantization != 8 && options.Quantization != 16 && options.Quantization != 32 && options.Quantization != 64 {
+		panic("Quantization must be one of 0, 4, 8, 16, 32, or 64")
+	}
+
+	c := &Collection{
+		CollectionOptions: options,
+		pivotsManager:     *newPivotsManager(distanceFn), // Use newPivotsManager
+	}
+
+	header := make([]byte, headerSize)
+
+	// Fill in the header
+	binary.BigEndian.PutUint32(header[0:], 1)                  // version
+	binary.BigEndian.PutUint32(header[4:], uint32(headerSize)) // length of the header
+	header[8] = byte(options.DistanceMethod)
+	binary.BigEndian.PutUint32(header[9:], uint32(options.DimensionCount))
+	header[13] = byte(options.Quantization) // Add this line
+
+	var err error
+	c.memfile, err = createMemFile(c.Name, header)
+	if err != nil {
+		panic(err)
+	}
+
+	return c
+}
+
+func (c *Collection) AddDocument(id uint64, vector []float64, metadata []byte) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	doc := &Document{
+		Vector:   vector,
+		Metadata: metadata,
+		ID:       id,
+	}
+
+	numDocs := len(c.memfile.idOffsets)
+
+	// Calculate the desired number of pivots using a logarithmic function
+	desiredPivots := int(math.Log2(float64(numDocs+1) - 7))
+
+	// Manage pivots
+	c.pivotsManager.ensurePivots(c, desiredPivots)
+
+	// Encode the document
+	encodedData := encodeDocument(doc, c.Quantization)
+
+	// Add or update the document in the memfile
+	c.memfile.addRecord(id, encodedData)
+
+	c.pivotsManager.pointAdded(doc)
 }
 
 func (c *Collection) GetDocument(id uint64) (*Document, error) {
@@ -83,8 +191,42 @@ func (c *Collection) getDocument(id uint64) (*Document, error) {
 	return doc, nil
 }
 
-func (c *Collection) getRandomID() (uint64, error) {
+func (c *Collection) UpdateDocument(id uint64, newMetadata []byte) error {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
 
+	// Read the existing record
+	data, err := c.memfile.readRecord(id)
+	if err != nil {
+		return err
+	}
+
+	// Decode the existing document
+	doc := c.decodeDocument(data, id)
+
+	// Update the metadata
+	doc.Metadata = newMetadata
+
+	// Encode the updated document
+	encodedData := encodeDocument(doc, c.Quantization)
+
+	// Update the document in the memfile
+	c.memfile.addRecord(id, encodedData)
+
+	return nil
+}
+
+func (c *Collection) removeDocument(id uint64) error {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	c.pivotsManager.pointRemoved(id)
+
+	// Remove the document from the memfile
+	return c.memfile.deleteRecord(id)
+}
+
+func (c *Collection) getRandomID() (uint64, error) {
 	if len(c.memfile.idOffsets) == 0 {
 		return 0, errors.New("no documents in the collection")
 	}
@@ -110,20 +252,6 @@ func (c *Collection) iterateDocuments(fn func(doc *Document)) {
 		doc := c.decodeDocument(data, id)
 		fn(doc)
 	}
-}
-
-// Helper function to compare two vectors for equality
-func equalVectors(vec1, vec2 []float64) bool {
-	if len(vec1) != len(vec2) {
-		return false
-	}
-	for i := range vec1 {
-		if vec1[i] != vec2[i] {
-			return false
-		}
-
-	}
-	return true
 }
 
 func (c *Collection) Search(args SearchArgs) SearchResults {
@@ -204,10 +332,10 @@ func (c *Collection) searchNearestNeighbours(args SearchArgs) SearchResults {
 	pointsSearched := 0
 
 	// Initialize heaps
-	approxHeap := &ApproxHeap{}
+	approxHeap := &approxHeap{}
 	heap.Init(approxHeap)
 
-	resultsHeap := &ResultHeap{}
+	resultsHeap := &resultHeap{}
 	heap.Init(resultsHeap)
 
 	// Calculate distances to pivots
@@ -224,12 +352,12 @@ func (c *Collection) searchNearestNeighbours(args SearchArgs) SearchResults {
 		}
 
 		minDistance := c.pivotsManager.approxDistance(args.Vector, id)
-		heap.Push(approxHeap, DistanceIndex{distance: minDistance, index: id})
+		heap.Push(approxHeap, distanceIndex{distance: minDistance, index: id})
 	}
 
 	// Process the approximate heap
 	for approxHeap.Len() > 0 {
-		item := heap.Pop(approxHeap).(DistanceIndex)
+		item := heap.Pop(approxHeap).(distanceIndex)
 
 		if resultsHeap.Len() == args.MaxCount && item.distance >= (*resultsHeap)[0].Distance {
 			break
@@ -268,173 +396,6 @@ func (c *Collection) searchNearestNeighbours(args SearchArgs) SearchResults {
 		Results:         results,
 		PercentSearched: float64(pointsSearched) / float64(len(c.memfile.idOffsets)) * 100,
 	}
-}
-
-func euclideanDistance(vec1, vec2 []float64) float64 {
-	sum := 0.0
-	for i := range vec1 {
-		diff := vec1[i] - vec2[i]
-		sum += diff * diff
-	}
-	return math.Sqrt(sum)
-}
-
-func cosineDistance(vec1, vec2 []float64) float64 {
-	dotProduct := 0.0
-	magnitude1 := 0.0
-	magnitude2 := 0.0
-	for i := range vec1 {
-		dotProduct += vec1[i] * vec2[i]
-		magnitude1 += vec1[i] * vec1[i]
-		magnitude2 += vec2[i] * vec2[i]
-	}
-	if magnitude1 == 0 || magnitude2 == 0 {
-		return 1.0 // Return max distance if one vector is zero
-	}
-	return 1.0 - (dotProduct / (math.Sqrt(magnitude1) * math.Sqrt(magnitude2)))
-}
-
-func (c *Collection) AddDocument(id uint64, vector []float64, metadata []byte) {
-	c.mutex.Lock()
-	defer c.mutex.Unlock()
-
-	doc := &Document{
-		Vector:   vector,
-		Metadata: metadata,
-		ID:       id,
-	}
-
-	numDocs := len(c.memfile.idOffsets)
-
-	// Calculate the desired number of pivots using a logarithmic function
-	desiredPivots := int(math.Log2(float64(numDocs+1) - 7))
-
-	// Manage pivots
-	c.pivotsManager.ensurePivots(c, desiredPivots)
-
-	// Encode the document
-	encodedData := encodeDocument(doc, c.Quantization)
-
-	// Add or update the document in the memfile
-	c.memfile.addRecord(id, encodedData)
-
-	c.pivotsManager.pointAdded(doc)
-}
-
-func (c *Collection) removeDocument(id uint64) error {
-	c.mutex.Lock()
-	defer c.mutex.Unlock()
-
-	c.pivotsManager.pointRemoved(id)
-
-	// Remove the document from the memfile
-	return c.memfile.deleteRecord(id)
-}
-
-func (c *Collection) UpdateDocument(id uint64, newMetadata []byte) error {
-	c.mutex.Lock()
-	defer c.mutex.Unlock()
-
-	// Read the existing record
-	data, err := c.memfile.readRecord(id)
-	if err != nil {
-		return err
-	}
-
-	// Decode the existing document
-	doc := c.decodeDocument(data, id)
-
-	// Update the metadata
-	doc.Metadata = newMetadata
-
-	// Encode the updated document
-	encodedData := encodeDocument(doc, c.Quantization)
-
-	// Update the document in the memfile
-	c.memfile.addRecord(id, encodedData)
-
-	return nil
-}
-
-type CollectionOptions struct {
-	Name           string
-	DistanceMethod int
-	DimensionCount int
-	Quantization   int
-}
-
-type Document struct {
-	ID       uint64
-	Vector   []float64
-	Metadata []byte
-}
-
-type SearchResult struct {
-	ID       uint64
-	Metadata []byte
-	Distance float64
-}
-
-type SearchResults struct {
-	Results []SearchResult
-
-	// percentage of database searched
-	PercentSearched float64
-}
-
-type SearchArgs struct {
-	Vector []float64
-	Filter FilterFn
-
-	// for nearest neighbour search
-	MaxCount int
-
-	// for radius search
-	Radius float64
-}
-
-type FilterFn func(id uint64, metadata []byte) bool
-
-// 4 bytes: version
-// 4 bytes: length of the header
-// 1 byte: distance method
-// 4 bytes: number of dimensions
-const headerSize = 14 // Update the header size to 14
-
-func NewCollection(options CollectionOptions) *Collection {
-	distanceFn := euclideanDistance
-	if options.DistanceMethod == Cosine {
-		distanceFn = cosineDistance
-	}
-
-	// Validate and set Quantization
-	if options.Quantization == 0 {
-		options.Quantization = 64
-	} else if options.Quantization != 4 && options.Quantization != 8 && options.Quantization != 16 && options.Quantization != 32 && options.Quantization != 64 {
-		panic("Quantization must be one of 0, 4, 8, 16, 32, or 64")
-	}
-
-	c := &Collection{
-		CollectionOptions: options,
-		pivotsManager:     *newPivotsManager(distanceFn), // Use newPivotsManager
-	}
-
-	header := make([]byte, headerSize)
-
-	// Fill in the header
-	binary.BigEndian.PutUint32(header[0:], 1)                  // version
-	binary.BigEndian.PutUint32(header[4:], uint32(headerSize)) // length of the header
-	header[8] = byte(options.DistanceMethod)
-	binary.BigEndian.PutUint32(header[9:], uint32(options.DimensionCount))
-	header[13] = byte(options.Quantization) // Add this line
-
-	var err error
-	c.memfile, err = createMemFile(c.Name, header)
-	if err != nil {
-		panic(err)
-	}
-
-	return c
 }
 
 func encodeDocument(doc *Document, quantization int) []byte {
@@ -476,23 +437,6 @@ func encodeDocument(doc *Document, quantization int) []byte {
 	copy(data[metadataOffset:], doc.Metadata)
 
 	return data
-}
-
-func getVectorSize(quantization int, dimensions int) int {
-	switch quantization {
-	case 4:
-		return (dimensions + 1) / 2
-	case 8:
-		return dimensions
-	case 16:
-		return dimensions * 2
-	case 32:
-		return dimensions * 4
-	case 64:
-		return dimensions * 8
-	default:
-		panic("Unsupported quantization level")
-	}
 }
 
 func (c *Collection) decodeDocument(data []byte, id uint64) *Document {
@@ -537,4 +481,58 @@ func (c *Collection) decodeDocument(data []byte, id uint64) *Document {
 		Vector:   vector,
 		Metadata: metadata,
 	}
+}
+
+func getVectorSize(quantization int, dimensions int) int {
+	switch quantization {
+	case 4:
+		return (dimensions + 1) / 2
+	case 8:
+		return dimensions
+	case 16:
+		return dimensions * 2
+	case 32:
+		return dimensions * 4
+	case 64:
+		return dimensions * 8
+	default:
+		panic("Unsupported quantization level")
+	}
+}
+
+// Helper function to compare two vectors for equality
+func equalVectors(vec1, vec2 []float64) bool {
+	if len(vec1) != len(vec2) {
+		return false
+	}
+	for i := range vec1 {
+		if vec1[i] != vec2[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func euclideanDistance(vec1, vec2 []float64) float64 {
+	sum := 0.0
+	for i := range vec1 {
+		diff := vec1[i] - vec2[i]
+		sum += diff * diff
+	}
+	return math.Sqrt(sum)
+}
+
+func cosineDistance(vec1, vec2 []float64) float64 {
+	dotProduct := 0.0
+	magnitude1 := 0.0
+	magnitude2 := 0.0
+	for i := range vec1 {
+		dotProduct += vec1[i] * vec2[i]
+		magnitude1 += vec1[i] * vec1[i]
+		magnitude2 += vec2[i] * vec2[i]
+	}
+	if magnitude1 == 0 || magnitude2 == 0 {
+		return 1.0 // Return max distance if one vector is zero
+	}
+	return 1.0 - (dotProduct / (math.Sqrt(magnitude1) * math.Sqrt(magnitude2)))
 }
