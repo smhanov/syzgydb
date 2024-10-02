c0484963da6deed7076f9d9b1a244b5785172158
Steve Hanov <steve.hanov@gmail.com>
2024-10-02 09:07:48 -0400

    refactor: simplify configuration loading and update default values in syzgydb package

--- syzgydb/config.go
+++ syzgydb/config.go
@@ -1,81 +1,55 @@
-package syzgydb
+package main
 
 import (
 	"fmt"
-	"log"
 
-	"sync"
-
+	"github.com/smhanov/syzgydb"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 )
 
-// Config holds the configuration settings for the service.
-type Config struct {
-	OllamaServer string `mapstructure:"ollama_server"`
-	TextModel    string `mapstructure:"text_model"`
-	ImageModel   string `mapstructure:"image_model"`
-}
-
-var GlobalConfig *Config
-var configMutex sync.Mutex
-
-/*
-Configure sets the global configuration for the syzgydb package.
-
-Parameters:
-- cfg: The configuration struct to set as the global configuration.
-*/
-func Configure(cfg Config) {
-	configMutex.Lock()
-	defer configMutex.Unlock()
-	GlobalConfig = &cfg
-}
-func LoadConfig() error {
-	// Set default values
-	viper.SetDefault("ollama_server", "localhost:8080")
-	viper.SetDefault("text_model", "default-text-model")
-	viper.SetDefault("image_model", "default-image-model")
-
+func init() {
 	// Bind command-line flags
 	pflag.String("ollama-server", "", "Hostname and port of the Ollama server")
 	pflag.String("text-model", "", "Name of the text embedding model")
 	pflag.String("image-model", "", "Name of the image embedding model")
 	pflag.String("config", "", "Path to the configuration file")
+}
+
+func LoadConfig() error {
+	// Set default values
+	viper.SetDefault("ollama_server", "localhost:11434")
+	viper.SetDefault("text_model", "all-minilm")
+	viper.SetDefault("image_model", "minicpm-v")
+
 	pflag.Parse()
 	viper.BindPFlags(pflag.CommandLine)
 
 	// Bind environment variables
 	viper.AutomaticEnv()
-	viper.SetEnvPrefix("APP")
+	viper.SetEnvPrefix("SYZGY")
 
 	// Read configuration file if specified
 	configFile := viper.GetString("config")
 	if configFile != "" {
 		viper.SetConfigFile(configFile)
 	} else {
-		viper.SetConfigName("config")
+		viper.SetConfigName("syzgy.conf")
 		viper.AddConfigPath(".")
 		viper.AddConfigPath("/etc/syzgydb/")
 	}
 
 	if err := viper.ReadInConfig(); err != nil {
-		log.Printf("Warning: Could not read config file: %v", err)
+		fmt.Printf("Using defaults and command line/environent options\n     (%v)\n", err)
 	}
 
 	// Unmarshal configuration into struct
-	var cfg Config
+	var cfg syzgydb.Config
 	if err := viper.Unmarshal(&cfg); err != nil {
-		return nil, fmt.Errorf("unable to decode into struct, %v", err)
-	}
-
-	// Validate required settings
-	if cfg.OllamaServer == "" {
-		return nil, fmt.Errorf("Ollama Server configuration is required")
+		return fmt.Errorf("unable to decode into struct, %v", err)
 	}
 
 	// Assign the loaded configuration to the global variable
-	GlobalConfig = &cfg
-
+	syzgydb.Configure(cfg)
 	return nil
 }
