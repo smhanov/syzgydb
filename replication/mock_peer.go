012250fc9f4c474289c969a57f6480439997c228
Steve Hanov <steve.hanov@gmail.com>
2024-10-12 23:11:32 -0400

    feat: add time package import and update Connect method in MockPeer

--- replication/mock_peer.go
+++ replication/mock_peer.go
@@ -1,39 +1,40 @@
 package replication
 
 import (
-    "sync"
+	"sync"
+	"time"
 )
 
 type MockPeer struct {
-    *Peer
-    connectCalled bool
-    mu            sync.Mutex
+	*Peer
+	connectCalled bool
+	mu            sync.Mutex
 }
 
 func NewMockPeer(url string) *MockPeer {
-    return &MockPeer{
-        Peer: NewPeer(url),
-    }
+	return &MockPeer{
+		Peer: NewPeer(url),
+	}
 }
 
 func (mp *MockPeer) Connect(jwtSecret []byte) {
-    mp.mu.Lock()
-    defer mp.mu.Unlock()
-    mp.connectCalled = true
-    // Simulate a successful connection without actually connecting
-    mp.SetConnection(&mockConnection{})
-    // Set lastActive to simulate a recent connection
-    mp.Peer.lastActive = time.Now()
+	mp.mu.Lock()
+	defer mp.mu.Unlock()
+	mp.connectCalled = true
+	// Simulate a successful connection without actually connecting
+	mp.SetConnection(&mockConnection{})
+	// Set lastActive to simulate a recent connection
+	mp.Peer.lastActive = time.Now()
 }
 
 func (mp *MockPeer) WasConnectCalled() bool {
-    mp.mu.Lock()
-    defer mp.mu.Unlock()
-    return mp.connectCalled
+	mp.mu.Lock()
+	defer mp.mu.Unlock()
+	return mp.connectCalled
 }
 
 type mockConnection struct{}
 
-func (mc *mockConnection) Close() error                   { return nil }
-func (mc *mockConnection) WriteMessage(int, []byte) error { return nil }
+func (mc *mockConnection) Close() error                      { return nil }
+func (mc *mockConnection) WriteMessage(int, []byte) error    { return nil }
 func (mc *mockConnection) ReadMessage() (int, []byte, error) { return 0, nil, nil }
